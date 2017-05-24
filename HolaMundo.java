fa2aeb763425eaca30a65e64c86bf8188db273b1
Luis Miguel Mateo <luismateo9594@gmail.com>
2017-05-23 21:00:42 -0400

    Create HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo{
+	public static void main(String [] args){
+		
+		System.out.println("Hola Mundo");
+		
+		//100046700 - Hairon de Jesus Reyes Reyes - S Note (Samsung)
+		
+	}
+}
